284e0e6d33aa3b4743b3122a825dd267d2baddc8
Rift2020 <69626219+Rift2020@users.noreply.github.com>
2021-12-30 12:53:50 +0800

    Add files via upload

--- 离散数学/离散数学.mm
+++ 离散数学/离散数学.mm
@@ -1,7 +1,7 @@
 <map version="freeplane 1.9.8">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <node TEXT="离散数学" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1640781671496"><hook NAME="MapStyle" background="#2e3440">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_nord_template.mm"/>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" fit_to_viewport="false"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
@@ -78,7 +78,9 @@
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
 <node TEXT="逻辑" POSITION="right" ID="ID_794138631" CREATED="1640781692269" MODIFIED="1640781718368"/>
-<node TEXT="集合" POSITION="right" ID="ID_1017614320" CREATED="1640781719214" MODIFIED="1640781727915">
+<node TEXT="*证明" POSITION="right" ID="ID_1367205515" CREATED="1640838691348" MODIFIED="1640838696715"/>
+<node TEXT="基本结构" POSITION="right" ID="ID_471278462" CREATED="1640838710180" MODIFIED="1640838713403">
+<node TEXT="集合" ID="ID_1017614320" CREATED="1640781719214" MODIFIED="1640781727915">
 <node TEXT="基本概念" ID="ID_1421703303" CREATED="1640784091425" MODIFIED="1640784095750">
 <node TEXT="定义" ID="ID_928928111" CREATED="1640784800953" MODIFIED="1640784804348">
 <node TEXT="\latex 集合A定义为一堆没有次序的对象\\若x是这堆对象中一个，则称x是A中元素\\记作$x\in A$,否则$x\not \in A$" ID="ID_1527664139" CREATED="1640783072094" MODIFIED="1640783369413"/>
@@ -113,17 +115,19 @@
 <node TEXT="如果你熟悉编程，可以感受到这是一个双循环的构造" ID="ID_960587780" CREATED="1640785691350" MODIFIED="1640785867320"/>
 <node TEXT="显然笛卡尔积集合的大小是两个集合大小的积，显然不满足交换律" ID="ID_1284523486" CREATED="1640786354596" MODIFIED="1640786424110"/>
 </node>
-<node TEXT="对于多个集合的笛卡尔积是符合直觉的" ID="ID_925455517" CREATED="1640786033013" MODIFIED="1640786079534">
+<node TEXT="*对于多个集合的笛卡尔积是符合直觉的" ID="ID_925455517" CREATED="1640786033013" MODIFIED="1640838567247">
 <node TEXT="\latex $A_1\times A_2 ...A_n=\{&lt;a_1,a_2,...,a_n&gt;|a_i\in A_i,i=1,2,...,n\}$" ID="ID_1915707918" CREATED="1640786080701" MODIFIED="1640786186180"/>
 </node>
 </node>
 </node>
 </node>
-<node TEXT="二元关系" POSITION="right" ID="ID_469793964" CREATED="1640786753903" MODIFIED="1640786760303">
+<node TEXT="二元关系" ID="ID_469793964" CREATED="1640786753903" MODIFIED="1640786760303">
 <node TEXT="\latex 若集合的所有元素都是有序对，\\则称集合为二元关系,记为R\\记$&lt;x,y&gt;\in R$为$xRy$" ID="ID_533113159" CREATED="1640786996407" MODIFIED="1640787538704"/>
 <node TEXT="\latex $A\times B$的任何子集定义的二元关系，\\称作A到B的二元关系\\A=B时，称作A上的二元关系" ID="ID_389190734" CREATED="1640787597571" MODIFIED="1640787815878"/>
 <node TEXT="关系矩阵，关系图" ID="ID_765250284" CREATED="1640788070690" MODIFIED="1640788090858">
-<node TEXT="若a,b有关系则矩阵M[a][b]=1,否则为0。若a,b有关系则连一条ab有向边" ID="ID_1504966179" CREATED="1640788092249" MODIFIED="1640788153734"/>
+<node TEXT="矩阵是0-1矩阵，图是有向图，若a,b有关系则矩阵M[a][b]=1,否则为0。若a,b有关系则连一条ab有向边" ID="ID_1504966179" CREATED="1640788092249" MODIFIED="1640834947582">
+<node TEXT="显然矩阵是方阵" ID="ID_257537093" CREATED="1640834584466" MODIFIED="1640834598518"/>
+</node>
 </node>
 <node TEXT="各种无聊的关系" ID="ID_140558489" CREATED="1640788077280" MODIFIED="1640788313283">
 <node TEXT="空关系，全域关系，恒等关系，小于等于关系，整除关系" ID="ID_1647460598" CREATED="1640788313481" MODIFIED="1640788364269"/>
@@ -133,10 +137,71 @@
 <node TEXT="取关系里所有x组成集合，取...y..,,  定义域和值域的并" ID="ID_1667216433" CREATED="1640789150682" MODIFIED="1640789242683"/>
 </node>
 <node TEXT="逆，合成，限制，像" ID="ID_373244972" CREATED="1640789322028" MODIFIED="1640789338568">
-<node TEXT="\latex 1.颠倒次序$F^{-1}=\{&lt;x,y&gt;|yFx\}$\\2.把两个二元关系接起来\\$F\circ G=\{&lt;x,y&gt;| \exists z(xGz且zFy) \}$\\3." ID="ID_1386604058" CREATED="1640790593833" MODIFIED="1640793483176"/>
+<node TEXT="\latex F,G是任意关系,A是集合1.颠倒次序$F^{-1}=\{&lt;x,y&gt;|yFx\}$\\2.把两个二元关系接起来\\$F\circ G=\{&lt;x,y&gt;| \exists z(xGz且zFy) \}$\\3.把F的定义域锁在A\\$F\upharpoonright A=\{&lt;x,y&gt;|xFy且x\in A\}$\\4.限制后的值域\\$F[A]=ran(F\upharpoonright A)$" ID="ID_1386604058" CREATED="1640790593833" MODIFIED="1640834531507"/>
+</node>
+<node TEXT="幂" ID="ID_1872848716" CREATED="1640834646378" MODIFIED="1640834653039">
+<node TEXT="\latex $R^0=\{&lt;x,x&gt;|x\in A\}$\\R^n=R^{n-1}\circ R,(n\geq 1)" ID="ID_1478325785" CREATED="1640834666738" MODIFIED="1640834732899">
+<node TEXT="关系的幂运算等同于{[(关系对应矩阵)的幂]对应的关系}" ID="ID_538266638" CREATED="1640834755989" MODIFIED="1640834816393">
+<node TEXT="因此关系的幂运算性质与矩阵幂一致" ID="ID_1333665824" CREATED="1640834828243" MODIFIED="1640835034271"/>
+</node>
+<node TEXT="*计算" ID="ID_1953554044" CREATED="1640835022844" MODIFIED="1640838586067">
+<node TEXT="我们可以使用矩阵快速幂加速运算" ID="ID_512968289" CREATED="1640835036277" MODIFIED="1640835038872"/>
 </node>
 </node>
 </node>
+</node>
+<node TEXT="关系的性质" ID="ID_391662526" CREATED="1640835196831" MODIFIED="1640835205891">
+<node TEXT="自反性，反自反性，对称性，反对称性，传递性" ID="ID_1370845935" CREATED="1640835208003" MODIFIED="1640835239561">
+<node TEXT="" ID="ID_368887243" CREATED="1640835239788" MODIFIED="1640835501815">
+<hook URI="images/关系性质对应表.jpg" SIZE="0.24567032" NAME="ExternalObject"/>
+<node TEXT="*计算" ID="ID_100608978" CREATED="1640835620154" MODIFIED="1640838589577">
+<node TEXT="\latex 显然我们可以$O(n)$检查(反)自反性\\$O(n^2)$检查(反)对称性\\传递性可以先计算传递闭包，然后判断是否一样" ID="ID_1273518333" CREATED="1640835625862" MODIFIED="1640836722965"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="关系的闭包" ID="ID_1094825500" CREATED="1640836056515" MODIFIED="1640836063084">
+<node TEXT="\latex 自反闭包$r(R)=R\cup R^0$\\对称闭包$s(R)=R\cup R^{-1}$\\传递闭包$t(R)=R\cup R^2\cup....$" ID="ID_1655051552" CREATED="1640836066006" MODIFIED="1640837031236">
+<node TEXT="\latex R自反则t(R),s(R)也自反,\\R对称则r(R),t(R)也对称,\\R传递则r(R)也传递,\textcolor {red}{s(R)未必传递}\\有tsr(R)=t(s(r(R)))具备自反性对称性和传递性" ID="ID_1534288567" CREATED="1640836968643" MODIFIED="1640837301102"/>
+<node TEXT="实际上，容易想到t(R)右边最多计算到n次(考虑图连成一条链，这是最坏情况)" ID="ID_1350949367" CREATED="1640836174663" MODIFIED="1640836234732"/>
+<node TEXT="*t(R)的计算" ID="ID_1207690084" CREATED="1640836275641" MODIFIED="1640838595399">
+<node TEXT="\latex 1.我们依旧可以利用矩阵快速幂的思想\\令$X_0=R$\\repeat($\lceil\log_2 n\rceil$):$X_n=X_{n-1}+X_{n-1}^2$\\$t(R)=X_{\lceil\log_2 n\rceil}$\\复杂度为$O(n^3 \log n)$\\2.可能爆搜(记忆化)好一点,目测是$O(n(n+m))$" ID="ID_1697825177" CREATED="1640836285160" MODIFIED="1640836704343"/>
+</node>
+</node>
+</node>
+<node TEXT="等价关系和偏序关系" ID="ID_1981042161" CREATED="1640836909209" MODIFIED="1640836922526">
+<node TEXT="设R是非空集合A上的关系，如果R自反，对称且传递，则称R是A上的等价关系" ID="ID_1142104130" CREATED="1640836943614" MODIFIED="1640837369232">
+<node TEXT="等价类" ID="ID_1119477917" CREATED="1640837701977" MODIFIED="1640837707008">
+<node TEXT="\latex 说人话就是，等于x的集合\\$[x]_R=\{y|y\in A且xRy\}$\\等价关系上才有等价类" ID="ID_1707076961" CREATED="1640837708918" MODIFIED="1640837793437"/>
+</node>
+<node TEXT="商集" ID="ID_953969864" CREATED="1640837824042" MODIFIED="1640837832598">
+<node TEXT="\latex 集合A上所有等价类的集合\\$A/R=\{[x]_R|x\in A\}$" ID="ID_652115758" CREATED="1640837832930" MODIFIED="1640837875105"/>
+</node>
+<node TEXT="划分" ID="ID_1454591336" CREATED="1640837916926" MODIFIED="1640837920138">
+<node TEXT="\latex 符合直觉的形如$\{\{a\},\{b,c\},\{d\}\}$叫划分\\若$\pi$是A的一个子集族且\\1.$\emptyset \not \in \pi$\\2.$\pi$中任意两个元素不交\\3.$\pi$中所有元素并集等于A\\则称\pi为A的划分,\pi的元素称为划分块" ID="ID_1995175337" CREATED="1640837920658" MODIFIED="1640838225348"/>
+</node>
+</node>
+<node TEXT="\latex 设R是非空集合A上的关系，如果R自反，\textcolor{red}{反对称}\\且传递，则称R是A上的偏序关系" ID="ID_1367729766" CREATED="1640837414749" MODIFIED="1640837454041">
+<node TEXT="全序" ID="ID_301989897" CREATED="1640838258105" MODIFIED="1640838329292">
+<node TEXT="符合直觉的，如果A中任意两个元素都可比，则称该偏序关系为全序关系" ID="ID_1967892753" CREATED="1640838329546" MODIFIED="1640838381985">
+<node TEXT="比如正整数中的小于等于关系就是全序的。但正整数的整除关系就仅仅只是偏序关系" ID="ID_483203859" CREATED="1640838382177" MODIFIED="1640838448512"/>
+</node>
+</node>
+<node TEXT="哈斯图" ID="ID_1649282735" CREATED="1640838481988" MODIFIED="1640838485509"/>
+<node TEXT="最元，极元，界" ID="ID_1085713586" CREATED="1640838499680" MODIFIED="1640838523632"/>
+</node>
+</node>
+</node>
+<node TEXT="*函数" ID="ID_441550461" CREATED="1640838534895" MODIFIED="1640838552210"/>
+</node>
+<node TEXT="*数论与密码学" POSITION="right" ID="ID_359950678" CREATED="1640838767622" MODIFIED="1640838792817"/>
+<node TEXT="*归纳与递归" POSITION="right" ID="ID_1917552756" CREATED="1640838815354" MODIFIED="1640838822296"/>
+<node TEXT="*计数技术与组合数学" POSITION="right" ID="ID_150326879" CREATED="1640838822551" MODIFIED="1640838873940"/>
 <node TEXT="图论" POSITION="right" ID="ID_1063695321" CREATED="1640781728188" MODIFIED="1640781730579"/>
+<node TEXT="*计算模型" POSITION="right" ID="ID_1833337611" CREATED="1640838937422" MODIFIED="1640838942419">
+<node TEXT="语言与文法" ID="ID_93862365" CREATED="1640838943219" MODIFIED="1640838949420"/>
+<node TEXT="状态机" ID="ID_1555364906" CREATED="1640838949993" MODIFIED="1640838970769"/>
+<node TEXT="t图灵机" ID="ID_129752820" CREATED="1640838970997" MODIFIED="1640838973366"/>
+</node>
 </node>
 </map>
